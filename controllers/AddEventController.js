02393aaa783d05227823f5a0acde59278bda8e40
pamudu-ratnayake <it19993416@my.sllit.lk>
2021-08-25 01:44:40 +0530

    Change Event POST

--- controllers/AddEventController.js
+++ controllers/AddEventController.js
@@ -1,5 +1,6 @@
 let AddEvents = require("../models/EventAdd");
 
+//-----POST-----
 exports.postAddEvent = (req, res, next) => {
   const event_name = req.body.event_name;
   const org_name = req.body.org_name;
