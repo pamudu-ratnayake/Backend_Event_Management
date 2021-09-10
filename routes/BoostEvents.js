b5a6fb542d3f3d706729658f746cc3d59be92f8e
Hansani-999 <it19965000@my.sliit.lk>
2021-09-11 01:11:33 +0530

    Changes on routes

--- routes/BoostEvents.js
+++ routes/BoostEvents.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 // const Advertisement = require("../models/Advertisement");
 const Controller = require("../controllers/Controller");
 
+//routes......
 router.post("/addadvertisement", Controller.addAdvertisement);
 
 router.get("/get/:id", Controller.viewoneAdvertisement);
