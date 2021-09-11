fbb746503b3f034a40d6422a81dfe145a257e4c5
Malith Madusankha <malithsony@gmail.com>
2021-09-11 11:35:49 +0530

    git pull dev

--- models/serviceProviderModels/company.js
+++ models/serviceProviderModels/company.js
@@ -7,18 +7,15 @@ const companySchema = new mongoose.Schema({
 	},
 	company_name: {
 		type: String,
-		// required: true,
 	},
 	service_provider_type: {
 		type: String,
-		// required: true,
 	},
 	details: {
 		type: String,
 	},
 	file: {
 		type: String,
-		// required: true,
 	},
 });
 
