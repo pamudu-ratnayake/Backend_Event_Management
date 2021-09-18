a96c7da0f3780abd8787d5fd698923ddc484b6e8
pamudu-ratnayake <it19993416@my.sliit.lk>
2021-09-18 22:02:30 +0530

    Begin Auth

---
+++ models/auth/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const authUser = new Schema({
+    firstName:{
+        type:String,
+        required: true,
+    },
+    lastName:{
+        type:String,
+        required: true,
+    },
+    email:{
+        type:String,
+        required: true,
+    },
+    password:{
+        type:String,
+        required: true,
+    },
+    type:{
+        type:String,
+        required: true,
+    },
+
+});
+
+const userAuth = mongoose.model("Users", authUser);
+
+module.exports = userAuth;
