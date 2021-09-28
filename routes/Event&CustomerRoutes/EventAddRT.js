6a2b4a155affbe48c27ddd623f4c285d917e6a33
pamudu-ratnayake <it19993416@my.sliit.lk>
2021-09-28 23:11:51 +0530

    event routes update

--- routes/Event&CustomerRoutes/EventAddRT.js
+++ routes/Event&CustomerRoutes/EventAddRT.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 let AddEvents = require("../../controllers/Event&CustomerControllers/AddEventController");
 
-const auth = require("../../middleware/auth")
+const auth = require("../../middleware/auth");
 
 //routes
 router.post("/addevent", AddEvents.postAddEvent);
-router.get("/getevents",auth, AddEvents.getAllEvents);
+router.get("/getevents", auth, AddEvents.getAllEvents);
 router.put("/updateevent/:event_id", AddEvents.updateEvent);
 router.delete("/deleteevent/:event_id", AddEvents.deleteEvent);
-router.get("/getOneEvent/:event_id", AddEvents. getOneEvent);
+router.get("/getOneEvent/:event_id", AddEvents.getOneEvent);
 
 module.exports = router;
